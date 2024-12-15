d0bfeadc8c58e11bc5a30bd882c1e6f911a5bb9a
odie <oopsdog@gmail.com>
2024-12-16 01:16:26 +0800

    init

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import *
+from linebot.models import MessageEvent
+from linebot.models import TextMessage, TextSendMessage
 import os
 
 app = Flask(__name__)
 
 line_bot_api = LineBotApi(os.getenv['CHANNEL_ACCESS_TOKEN'], None)
 handler = WebhookHandler(os.getenv['CHANNEL_SECRET'], None)
-parser = WebhookParser(os.environ['CHANNEL_SECRET'])
+##parser = WebhookParser(os.environ['CHANNEL_SECRET'])
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -21,10 +22,34 @@ def callback():
         abort(400)
     return 'OK'
 
+##@handler.add(MessageEvent, message=TextMessage)
+##def handle_message(event):
+##    message1 = event.message.text
+##    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=message1))
+
 @handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    message1 = event.message.text
-    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=message1))
+def handle_text_message(event):
+
+    # 當 LINE 後台發送測試訊號過來時，會使用一組假 token，無視它就好
+    if event.reply_token == '0' * 32:
+        return
+
+    # 暫停 1.5 秒，假裝在打字或讀訊息
+    time.sleep(1.5)
+
+    # 隨機回覆一串敷衍訊息
+    linebot_client.reply_message(
+        event.reply_token,
+        TextSendMessage(
+            random.choice([
+                '好',
+                'ok',
+                '恩～',
+                '我知道了',
+            ])
+        )
+    )
+
 
 import os
 if __name__ == "__main__":
