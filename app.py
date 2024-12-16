000c4de2a33ea2249be39188c11c531197b2b659
odie <oopsdog@gmail.com>
2024-12-16 08:22:02 +0800

    init

--- app.py
+++ app.py
@@ -4,11 +4,14 @@ from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent
 from linebot.models import TextMessage, TextSendMessage
 import os
+import json
+import openai
 
 app = Flask(__name__)
 
 line_bot_api = LineBotApi(os.environ.get('CHANNEL_ACCESS_TOKEN'))
 handler = WebhookHandler(os.environ.get('CHANNEL_SECRET'))
+##openai.api_key = os.environ.get('OPEN_AI_SECRET')
 ##parser = WebhookParser(os.environ['CHANNEL_SECRET'])
 
 @app.route("/callback", methods=['POST'])
@@ -25,7 +28,22 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message1 = event.message.text
-    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=message1))
+    ai_msg = message1[:6].lower()
+        reply_msg = ''
+        if ai_msg == 'hi ai:':
+            openai.api_key = os.environ.get('OPEN_AI_SECRET')
+            # 將第六個字元之後的訊息發送給 OpenAI
+            response = openai.Completion.create(
+                model='text-davinci-003',
+                prompt=msg[6:],
+                max_tokens=256,
+                temperature=0.5,
+                )
+            # 接收到回覆訊息後，移除換行符號
+            reply_msg = response["choices"][0]["text"].replace('\n','')
+        else:
+            reply_msg = message1
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_msg))
 
 
 
