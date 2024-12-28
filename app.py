df0de22de87fd01ebe2f5a109fa7b9b71574dcad
odie <oopsdog@gmail.com>
2024-12-28 15:26:35 +0800

    0062

--- app.py
+++ app.py
@@ -110,7 +110,7 @@ def handle_message(event):
         qmsg = qmsg + 'The item 1 is the time 6 hr before now, now, and 6 hr after now. And the item2 is the flood condition.\n'
         qmsg = qmsg + 'Generate the flood warning regarding to the status before, now and later.\n'
         qmsg = qmsg + 'Give the receiver some instrutions to do for now and later.\n'
-        qmsg = qmsg + 'The format of this warning is the topic date and time as the header, and the status and the actions to do.\n'
+        qmsg = qmsg + 'The format of this warning is the topic date and time as the header, and the status and the actions (instructions) to do.\n'
         qmsg = qmsg + 'the date and time is '+ str(nowtime) +' now\n'
         qmsg = qmsg + 'Complete the message in 150 words.\n'
 
