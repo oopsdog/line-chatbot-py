61e6a948ce892e1e4fc93fa73165c59638b64f3e
odie <oopsdog@gmail.com>
2024-12-28 14:18:37 +0800

    0050

--- app.py
+++ app.py
@@ -63,10 +63,10 @@ def handle_message(event):
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_msg))
 
     elif ai_msg.startswith('COND'):
-        match = re.search(r"COND([A-Z]{3})", text)
-        bef = match.group(0)
-        now = match.group(1)
-        aft = match.group(2)
+        match = re.search(r"COND([A-Z]{3})", ai_msg)
+        bef = match.group(1)[0]
+        now = match.group(1)[1]
+        aft = match.group(1)[2]
         test_msg = bef + now + aft + " test test"
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=test_msg))
     elif ai_msg.startswith('Hi'):
